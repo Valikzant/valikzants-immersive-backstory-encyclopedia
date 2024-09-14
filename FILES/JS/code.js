55234ae1d61010dcd955c4865bf609378243bdf5
Valikzant <Valikzant@mail.ru>
2024-09-14 22:45:25 +0300

    Изменение стиля и других штучичек.

--- FILES/JS/code.js
+++ FILES/JS/code.js
@@ -1,10 +1,16 @@
 
 document.addEventListener('DOMContentLoaded', function() {
-    const expandableSections = document.querySelectorAll('.BODY_SECTION.EXPANDABLE');
-    
-    expandableSections.forEach(section => {
+    const expandableContent = document.querySelectorAll('.SECTION_CONTENT.EXPANDABLE');
+
+    expandableContent.forEach(section => {
         section.addEventListener('click', function() {
             this.classList.toggle('EXPANDED');
         });
     });
 });
+
+function scrollToSection(sectionId) {
+    const section = document.getElementById(sectionId);
+    section.scrollIntoView({ behavior: 'smooth' });
+    window.scrollBy(0, -75);
+}
